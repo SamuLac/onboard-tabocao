e0e2d68a3e87d903f63d2b7eb47d7b29f223388a
Samuel Lacerda <samuel.lacerdaif@gmail.com>
2025-06-26 13:15:38 -0300

    feat: Create External Model

---
+++ models/external_model.py
@@ -0,0 +1,14 @@
+from datetime import datetime, timezone
+
+from db import db
+
+
+class ExternalDataModel(db.Model):
+    __tablename__ = 'external_data'
+
+    id = db.Column(db.Integer, primary_key=True)
+    origin = db.Column(db.string, nullable=False)
+    content = db.Column(db.JSON, nullable=False)
+
+    created_at = db.Column(db.DateTime(timezone=True), default=datetime.now(timezone.utc), nullable=False)
+    updated_at = db.Column(db.DateTime(timezone=True), default=datetime.now(timezone.utc), onupdate=datetime.now(timezone.utc), nullable=False)
