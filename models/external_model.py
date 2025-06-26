afe9748b4ad520d2aaf18440eae4300876f121f1
Samuel Lacerda <samuel.lacerdaif@gmail.com>
2025-06-26 13:39:51 -0300

    fix: Fix db.String in external_model

--- models/external_model.py
+++ models/external_model.py
@@ -7,7 +7,7 @@ class ExternalDataModel(db.Model):
     __tablename__ = 'external_data'
 
     id = db.Column(db.Integer, primary_key=True)
-    origin = db.Column(db.string, nullable=False)
+    origin = db.Column(db.String, nullable=False)
     content = db.Column(db.JSON, nullable=False)
 
     created_at = db.Column(db.DateTime(timezone=True), default=datetime.now(timezone.utc), nullable=False)
